dbbb556fee90c5c7a16eeea297aa6837792488f7
JM <j.maxwell.alan@gmail.com>
2023-01-15 00:26:21 -0500

    reorder primary type enum declaration

--- src/content_type/primary_type.rs
+++ src/content_type/primary_type.rs
@@ -7,10 +7,10 @@ pub enum ContentType {
   Application(ApplicationSubtype),
   Text(TextSubtype),
   Audio(AudioSubtype),
-  Font(FontSubtype),
   Image(ImageSubtype),
-  Model(ModelSubtype),
   Video(VideoSubtype),
+  Font(FontSubtype),
+  Model(ModelSubtype),
   Example(ExampleSubtype),
 }
 
@@ -27,17 +27,17 @@ impl From<ContentType> for String {
       ContentType::Audio(audio_subtype) => {
         format!("audio/{}", audio_subtype.to_string()).to_string()
       }
-      ContentType::Font(font_subtype) => {
-        format!("font/{}", font_subtype.to_string()).to_string()
-      }
       ContentType::Image(image_subtype) => {
         format!("image/{}", image_subtype.to_string()).to_string()
       }
-      ContentType::Model(model_subtype) => {
-        format!("model/{}", model_subtype.to_string()).to_string()
-      }
       ContentType::Video(video_subtype) => {
         format!("video/{}", video_subtype.to_string()).to_string()
+      }
+      ContentType::Font(font_subtype) => {
+        format!("font/{}", font_subtype.to_string()).to_string()
+      }
+      ContentType::Model(model_subtype) => {
+        format!("model/{}", model_subtype.to_string()).to_string()
       }
       ContentType::Example(example_subtype) => {
         format!("example/{}", example_subtype.to_string()).to_string()
