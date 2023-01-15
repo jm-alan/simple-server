9bfd9e7062eb1265e27d36c4fb4cfa9c2acd1cd4
JM <j.maxwell.alan@gmail.com>
2023-01-14 20:00:27 -0500

    move content type out of response to headers

--- src/response/mod.rs
+++ src/response/mod.rs
@@ -1,19 +1,17 @@
-mod content_type;
 mod http_body;
 mod http_version;
 mod response_to_string;
 mod status_reason;
 
 use self::{
-  content_type::ContentType, http_body::HttpBody, http_version::HttpVersion,
-  status_reason::StatusReason,
+  http_body::HttpBody, http_version::HttpVersion, status_reason::StatusReason,
 };
 
 #[derive(Debug, Default, Clone)]
 pub struct Response {
   http_version: HttpVersion,
   status_reason: StatusReason,
-  content_type: ContentType,
+  headers: Vec<String>,
   body: HttpBody,
 }
 
