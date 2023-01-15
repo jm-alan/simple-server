0defc417014de4a507093fc76ef854661a07477a
JM <j.maxwell.alan@gmail.com>
2023-01-14 20:00:44 -0500

    add additional linebreak newline to response stringify

--- src/response/response_to_string.rs
+++ src/response/response_to_string.rs
@@ -3,10 +3,10 @@ use super::Response;
 impl ToString for Response {
   fn to_string(&self) -> String {
     format!(
-      "{} {}\r\n{}\r\n",
+      "{} {}\r\n\r\n\r\n{}\r\n",
       self.http_version.to_string(),
       self.status_reason.to_string(),
-      self.body.to_string()
+      self.body.to_string(),
     )
   }
 }
