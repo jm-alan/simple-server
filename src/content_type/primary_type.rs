5b1c4d520e0046fa255b9a0467b3f84f07931424
JM <j.maxwell.alan@gmail.com>
2023-01-15 00:11:36 -0500

    add subtype placeholders to other primary content types

--- src/content_type/primary_type.rs
+++ src/content_type/primary_type.rs
@@ -5,11 +5,49 @@ pub enum ContentType {
   #[default]
   None,
   Application(ApplicationSubtype),
-  Text,
-  Audio,
-  Font,
-  Image,
-  Model,
-  Video,
-  Example,
+  Text(TextSubtype),
+  Audio(AudioSubtype),
+  Font(FontSubtype),
+  Image(ImageSubtype),
+  Model(ModelSubtype),
+  Video(VideoSubtype),
+  Example(ExampleSubtype),
+}
+
+impl From<ContentType> for String {
+  fn from(content_type: ContentType) -> Self {
+    match content_type {
+      ContentType::None => "".to_string(),
+      ContentType::Application(subtype) => {
+        format!("application/{}", subtype.to_string()).to_string()
+      }
+      ContentType::Text(text_subtype) => {
+        format!("text/{}", text_subtype.to_string()).to_string()
+      }
+      ContentType::Audio(audio_subtype) => {
+        format!("audio/{}", audio_subtype.to_string()).to_string()
+      }
+      ContentType::Font(font_subtype) => {
+        format!("font/{}", font_subtype.to_string()).to_string()
+      }
+      ContentType::Image(image_subtype) => {
+        format!("image/{}", image_subtype.to_string()).to_string()
+      }
+      ContentType::Model(model_subtype) => {
+        format!("model/{}", model_subtype.to_string()).to_string()
+      }
+      ContentType::Video(video_subtype) => {
+        format!("video/{}", video_subtype.to_string()).to_string()
+      }
+      ContentType::Example(example_subtype) => {
+        format!("example/{}", example_subtype.to_string()).to_string()
+      }
+    }
+  }
+}
+
+impl ToString for ContentType {
+  fn to_string(&self) -> String {
+    self.clone().into()
+  }
 }
