379f27b447d87c3d9e5cce43eb581de720d3c400
JM <j.maxwell.alan@gmail.com>
2023-12-10 07:26:18 -0500

    format headers in response serialization

--- src/response/response_to_string.rs
+++ src/response/response_to_string.rs
@@ -3,11 +3,28 @@ use super::Response;
 impl ToString for Response {
   #[inline(always)]
   fn to_string(&self) -> String {
+    let formatted_headers = if self.headers.len() > 0 {
+      self
+        .headers
+        .iter()
+        .map(|[key, val]| format!("{key}: {val}\r\n"))
+        .collect::<Vec<_>>()
+        .join("")
+    } else {
+      "".into()
+    };
+
+    let formatted_body = match self.body {
+      Some(body) => body.to_string(),
+      _ => "\r\n".to_string(),
+    };
+
     format!(
-      "{} {}\r\n\r\n\r\n{}\r\n",
+      "{} {}\r\n{}{}",
       self.http_version.to_string(),
       self.status_reason.to_string(),
-      self.body.to_string(),
+      formatted_headers,
+      formatted_body,
     )
   }
 }
