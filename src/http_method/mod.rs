79ea1225be871c69792f7b012881914992c9f0f5
JM <j.maxwell.alan@gmail.com>
2023-12-10 07:53:31 -0500

    impl ToString for HttpMethod

--- src/http_method/mod.rs
+++ src/http_method/mod.rs
@@ -11,3 +11,19 @@ pub enum HttpMethod {
   TRACE,
   PATCH,
 }
+
+impl ToString for HttpMethod {
+  fn to_string(&self) -> String {
+    match self {
+      Self::GET => "GET".to_string(),
+      Self::HEAD => "HEAD".to_string(),
+      Self::POST => "POST".to_string(),
+      Self::PUT => "PUT".to_string(),
+      Self::DELETE => "DELETE".to_string(),
+      Self::CONNECT => "CONNECT".to_string(),
+      Self::OPTIONS => "OPTIONS".to_string(),
+      Self::TRACE => "TRACE".to_string(),
+      Self::PATCH => "PATCH".to_string(),
+    }
+  }
+}
