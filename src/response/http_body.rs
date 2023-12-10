ad3792e478adcf78f070ff1ea8438805fe355a40
JM <j.maxwell.alan@gmail.com>
2023-12-10 07:20:15 -0500

    actually finish v1 http body tostring implementation

--- src/response/http_body.rs
+++ src/response/http_body.rs
@@ -3,11 +3,17 @@ use crate::content_type::ContentType;
 #[derive(Debug, Default, Clone)]
 pub struct HttpBody {
   content_type: ContentType,
-  raw_data: Box<u8>,
+  raw_data: String,
 }
 
 impl ToString for HttpBody {
+  #[inline(always)]
   fn to_string(&self) -> String {
-    todo!()
+    format!(
+      "Content-Type: {}\r\nContent-Length: {}\r\n\r\n{}",
+      self.content_type.to_string(),
+      self.raw_data.len(),
+      self.raw_data
+    )
   }
 }
