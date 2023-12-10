b203b836abe1d4d06d6a1852922b4034b272e16e
JM <j.maxwell.alan@gmail.com>
2023-12-10 07:31:46 -0500

    add method, uri, http version to request

--- src/request/mod.rs
+++ src/request/mod.rs
@@ -1,6 +1,11 @@
+use crate::http_version::HttpVersion;
+
 mod request_from_tcp_stream;
 
-#[derive(Debug, Default, Clone)]
+#[derive(Debug, Clone)]
 pub struct Request {
   raw_components: Vec<String>,
+  method: String,
+  uri: String,
+  http_version: HttpVersion,
 }
