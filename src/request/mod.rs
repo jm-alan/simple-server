7c51f869fad70310f230587f30f7cae55768e64d
JM <j.maxwell.alan@gmail.com>
2023-12-12 06:11:10 -0500

    make request constituents public

--- src/request/mod.rs
+++ src/request/mod.rs
@@ -1,11 +1,16 @@
-use crate::{http_method::HttpMethod, http_version::HttpVersion};
+use crate::{
+  http_method::HttpMethod, http_version::HttpVersion, types::Headers,
+};
 
+mod builder;
 mod request_from_tcp_stream;
+mod start_line;
 
 #[derive(Debug, Clone)]
 pub struct Request {
-  raw_components: Vec<String>,
-  method: HttpMethod,
-  uri: String,
-  http_version: HttpVersion,
+  pub method: HttpMethod,
+  pub uri: String,
+  pub http_version: HttpVersion,
+  pub headers: Option<Headers>,
+  pub body: Option<String>,
 }
