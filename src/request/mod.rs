3cc27ca0ba392c8d19694410ec6619009645249b
JM <j.maxwell.alan@gmail.com>
2023-12-15 13:00:29 -0500

    simplify request struct using StartLine

--- src/request/mod.rs
+++ src/request/mod.rs
@@ -1,16 +1,14 @@
-use crate::{
-  http_method::HttpMethod, http_version::HttpVersion, types::Headers,
-};
+use crate::types::Headers;
 
 mod builder;
 mod request_from_tcp_stream;
 mod start_line;
 
+pub use start_line::StartLine;
+
 #[derive(Debug, Clone)]
 pub struct Request {
-  pub method: HttpMethod,
-  pub uri: String,
-  pub http_version: HttpVersion,
-  pub headers: Option<Headers>,
+  pub start_line: StartLine,
+  pub headers: Headers,
   pub body: Option<String>,
 }
