38101d2bf5dda5419b17a0b0db511216f9302ff5
JM <j.maxwell.alan@gmail.com>
2023-12-16 19:14:25 -0500

    implement req from tcpstream

--- src/request/request_from_tcp_stream.rs
+++ src/request/request_from_tcp_stream.rs
@@ -3,26 +3,44 @@ use std::{
   net::TcpStream,
 };
 
-use crate::{http_method::HttpMethod, http_version::HttpVersion};
+use crate::utils::parse::{parse_body, parse_headers, parse_start_line};
 
-use super::Request;
+use super::{Request, StartLine};
 
-impl From<&mut TcpStream> for Request {
+impl Request {
   #[inline(always)]
-  fn from(mut stream: &mut TcpStream) -> Self {
-    let raw_components: Vec<String> = BufReader::new(&mut stream)
-      .lines()
-      .map(|buf_result| buf_result.unwrap_or("\r\n".into()))
-      .take_while(|line| !line.is_empty())
-      .collect();
+  pub fn from_tcp(stream: &TcpStream) -> Option<Self> {
+    let mut reader = BufReader::with_capacity(16384, stream);
 
-    println!("{:#?}", raw_components);
+    let headers_max = reader.fill_buf().ok()?;
+    let (raw_start_line, maybe_headers) = parse_start_line(headers_max)?;
+    let headers_and_remainder = parse_headers(&maybe_headers)?;
+    let start_line = StartLine::from_bytes(&raw_start_line)?;
 
-    Self {
-      raw_components,
-      method: HttpMethod::GET,
-      uri: "".to_string(),
-      http_version: HttpVersion::Modern,
-    }
+    let headers = headers_and_remainder.0;
+
+    let body = match (
+      start_line.method.permits_body(),
+      headers.iter().find(|(key, _)| {
+        key == "content-length"
+          || key == "Content-Length"
+          || key == "CONTENT-LENGTH"
+      }),
+    ) {
+      (false, _) | (_, None) => None,
+      (_, Some((_, raw_content_length))) => {
+        let init = headers_and_remainder.1;
+        let consumed_len = headers_max.len();
+        reader.consume(consumed_len);
+
+        parse_body(init, raw_content_length, &mut reader)
+      }
+    };
+
+    Some(Self {
+      start_line,
+      headers,
+      body,
+    })
   }
 }
