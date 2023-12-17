841a98e189b63d910c3110d803c64ee40250a504
JM <j.maxwell.alan@gmail.com>
2023-12-16 19:15:27 -0500

    convert body representation from string to raw bytes

--- src/request/mod.rs
+++ src/request/mod.rs
@@ -1,14 +1,13 @@
 use crate::types::Headers;
 
-mod builder;
 mod request_from_tcp_stream;
 mod start_line;
 
 pub use start_line::StartLine;
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 pub struct Request {
   pub start_line: StartLine,
   pub headers: Headers,
-  pub body: Option<String>,
+  pub body: Option<Vec<u8>>,
 }
